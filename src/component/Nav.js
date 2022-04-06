4984edddfad310706c01f6bde4bf3a4933eaf0c7
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-04-06 11:56:31 +0000

    add gh pages url to nav link

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,10 +1,11 @@
 const docsUrl = "https://github.com/streamich/react-use/blob/master/docs/useGeolocation.md";
+const ghUrl = "https://devmegan.github.io/geolocation"
 
 export default function Nav() {
     return(
         <nav className="navbar bg-primary">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl" href="/">useGeolocation</a>
+                <a className="btn btn-ghost normal-case text-xl" href={ghUrl}>useGeolocation</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
