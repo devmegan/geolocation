6af6541f9ca177dd4eade60a99fdaa76481757e1
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-04-06 10:47:25 +0000

    fix react attr names

--- src/component/Error.js
+++ src/component/Error.js
@@ -4,7 +4,7 @@ export default function Error({error, setError}) {
             <div className="mx-auto">
                 <button onClick={() => setError(false)}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                 </button>
                 <span>{error}</span>
